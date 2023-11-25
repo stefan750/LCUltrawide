6ae14f92ed9b984920457d43df34cf3fc80d1cad
stefan750 <7531670+stefan750@users.noreply.github.com>
2023-11-25 01:41:31 +0100

    Initial release

---
+++ Plugin.cs
@@ -0,0 +1,111 @@
+﻿using BepInEx;
+using HarmonyLib;
+using GameNetcodeStuff;
+using UnityEngine;
+using BepInEx.Configuration;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+namespace LCUltrawide
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        private static ConfigEntry<int> configResW;
+        private static ConfigEntry<int> configResH;
+
+        private static ConfigEntry<float> configUIScale;
+        private static ConfigEntry<float> configUIAspect;
+
+        private void Awake()
+        {
+            // Plugin startup logic
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+
+            configResW = Config.Bind("Resolution", "Width", 860, "Horizontal rendering resolution");
+            configResH = Config.Bind("Resolution", "Height", 520, "Vertical rendering resolution");
+            
+            configUIScale = Config.Bind("UI", "Scale", 3.2f, "Changes the size of UI elements on the screen");
+            configUIAspect = Config.Bind("UI", "AspectRatio", 1.77f, "Changes the aspect ratio of the ingame HUD, a higher number makes the HUD wider (4:3 = 1.33, 16:9 = 1.77, 21:9 = 2.33, 32:9 = 3.55)");
+
+            Harmony.CreateAndPatchAll( typeof( Plugin ) );
+        }
+
+        [HarmonyPatch(typeof(PlayerControllerB), "Start")]
+        [HarmonyPrefix]
+        static void PlayerControllerStart(PlayerControllerB __instance)
+        {
+            //Change camera render texture resolution
+            RenderTexture screenTex = __instance.gameplayCamera.targetTexture;
+
+            screenTex.width = configResW.Value;
+            screenTex.height = configResH.Value;
+
+            //Correct aspect ratio for camera view
+            GameObject panelObject = GameObject.Find("Systems/UI/Canvas/Panel");
+            if(panelObject != null)
+            {
+                if(panelObject.TryGetComponent<AspectRatioFitter>(out AspectRatioFitter arf))
+                {
+                    arf.aspectRatio = configResW.Value / (float)configResH.Value;
+                }
+            }
+
+            //Change UI scale
+            GameObject canvasObject = GameObject.Find("Systems/UI/Canvas");
+            if(canvasObject.TryGetComponent<Canvas>(out Canvas canvas))
+            {
+                canvas.scaleFactor = configUIScale.Value;
+            }
+
+            //Change HUD aspect ratio
+            GameObject hudObject = GameObject.Find("Systems/UI/Canvas/IngamePlayerHUD");
+            if (hudObject != null)
+            {
+                if (hudObject.TryGetComponent<AspectRatioFitter>(out AspectRatioFitter arf))
+                {
+                    arf.aspectRatio = configUIAspect.Value;
+                }
+            }
+
+            //Fix Inventory position
+            GameObject inventoryObject = GameObject.Find("Systems/UI/Canvas/IngamePlayerHUD/Inventory");
+            if (inventoryObject != null)
+            {
+                if (inventoryObject.TryGetComponent<RectTransform>(out RectTransform rectTransform))
+                {
+                    rectTransform.anchoredPosition = Vector2.zero;
+                    rectTransform.anchorMax = new Vector2(0.5f, 0f);
+                    rectTransform.anchorMin = new Vector2(0.5f, 0.5f);
+                    rectTransform.pivot = new Vector2(0.5f, 0f);
+                }
+            }
+        }
+
+        [HarmonyPatch(typeof(HUDManager), "UpdateScanNodes")]
+        [HarmonyPostfix]
+        static void HUDManagerUpdateScanNodes(PlayerControllerB playerScript, HUDManager __instance)
+        {
+            //Correct UI marker positions for scanned objects
+            RectTransform[] scanElements = __instance.scanElements;
+            Dictionary<RectTransform, ScanNodeProperties> scanNodes = Traverse.Create(__instance).Field("scanNodes").GetValue() as Dictionary<RectTransform, ScanNodeProperties>;
+
+            GameObject playerScreen = GameObject.Find("Systems/UI/Canvas/Panel/GameObject/PlayerScreen");
+            if(!playerScreen.TryGetComponent<RectTransform>(out RectTransform screenTransform))
+            {
+                return;
+            }
+            Rect rect = screenTransform.rect;
+
+            for (int i = 0; i < scanElements.Length; i++)
+            {
+                if(scanNodes.TryGetValue(scanElements[i], out ScanNodeProperties scanNode))
+                {
+                    Vector3 viewportPos = playerScript.gameplayCamera.WorldToViewportPoint(scanNode.transform.position);
+                    scanElements[i].anchoredPosition = new Vector2(rect.xMin + rect.width * viewportPos.x, rect.yMin + rect.height * viewportPos.y);
+                }
+            }
+        }
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,28 @@
+# LCUltrawide
+
+Lethal Company is locked to 16:9 aspect ratio by default and will add black bars on the sides of ultrawide monitors.  
+This mod makes some changes to the games rendering and UI to enable better support for any custom resolution and aspect ratio.
+
+## Features
+
+- Modify the games rendering resolution with full support for aspect ratios other than 16:9
+- Improve visibility by increasing the resolution (at the cost of performance)
+- Allows changing of HUD scale and aspect ratio
+- Fixes the inventory slots being slightly misaligned on some monitors
+- More robust code for the Scanner HUD to ensure correct position of markers
+
+
+## Installation
+
+1. Make sure you have **BepInEx** installed for the game (see https://thunderstore.io/c/lethal-company/p/BepInEx/BepInExPack/)
+2. Download the latest release of the mod from the releases page
+3. Navigate to the games install folder (you can right click the game in your Steam library, select "Manage" and then "Browse Local Files" to easily find it)
+4. Copy the downloaded **LCUltrawide.dll** to the **BepInEx/plugins/** folder
+5. Launch the game once with the mod installed to generate the config file
+
+## Usage
+
+1. Open the config file located under **BepInEx/config/LCUltrawide.cfg** inside the games install directory.  
+2. Change the **Width** and **Height** values under the **\[Resolution\]** section to your preferred resolution and aspect ratio.  
+    NOTE: the games default render resolution is 860x520, increasing it too far beyond these values can cause performance issues and will remove the "pixelated" style of the game. It is recommended to leave the Height at its default and only adjust the Width according to your monitors aspect ratio for best results.
+3. (Optional) Change the values under the **\[UI\]** section to adjust the scale and aspect ratio of the ingame HUD.
